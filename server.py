3b8ca0f7818926bcfafc11aace9c6efe1ecd6895
Lin Xin <furjen@hotmail.com>
2018-09-05 13:27:48 +0800

    multi-complete ready to modify to gevent

--- server.py
+++ server.py
@@ -7,15 +7,15 @@ test module
 
 __author__ = 'Lin Xin'
 
-import sys
+from gevent import monkey
+monkey.patch_all()
+import gevent
 import json
-import pygame
-from pygame.locals import *
 from socket import socket
 from time import sleep
 from threading import Thread
-from multiprocessing import Process, Pipe
-from single_snake import window_init, draw_line, MyThread, event_handle, Snake
+from multiprocessing import Process
+from single_snake import Snake
 
 
 class Server(socket):
@@ -26,49 +26,39 @@ class Server(socket):
         self.bind(host)
         self.listen(100)
         self.forever_serve()
-
-    @classmethod
-    def _init_game(cls, fd1):
-        cls.snakes_list = {}
-        cls.game_panel = window_init()
-        fd1.send(cls.game_panel)
-        while True:
-            cls.game_panel.fill((0, 0, 0))
-            draw_line(cls.game_panel)
-            for event in pygame.event.get(KEYDOWN):
-                if event.type == pygame.QUIT:
-                    sys.exit(0)
-            pygame.display.update()
+        print("waiting for connect")
 
     def forever_serve(self):
-        fd1, fd2 = Pipe()
-        game = Process(target=self._init_game, args=(fd1,))
-        game.start()
+        client = Process(target=self._handle_link)
+        client.daemon = True
+        client.start()
         while True:
-            print("waiting for connect")
+            sleep(0.3)
+            for snake in Server.snakes_list.values():
+                snake.move()
+            for client in self.snakes_list:
+                client.send(json.dumps(self.snakes_list.values()).encode())
+
+    def _handle_link(self):
+        while True:
             client, addr = self.accept()
             print('connect from {}'.format(client.getpeername()))
-            t = Thread(target=Server.client_handle, args=(client, fd2))
+            t = Thread(target=Server.client_handle, args=(client,))
             t.setDaemon(True)
             t.start()
 
-    @classmethod
-    def client_handle(cls, client, fd2):
+    def client_handle(self, client):
         print(client.recv(100).decode())
         client_snake = Snake()
-        client_snake.display(fd2.recv())
-        Server.snakes_list[client.getsockname()] = client_snake
-        client.send(json.dumps((3, 2, 1)).encode())
+        Server.snakes_list[client] = client_snake
         while True:
-            command = json.loads(client.recv(100).decode())
-            cls.snakes_list[client].move(command.key)
-            pygame.display.update()
+            command = client.recv(3).decode()
+            self.snakes_list[client].control(command)
 
 
 def main():
     server = Server()
     server.forever_serve()
-
 
 
 if __name__ == '__main__':
